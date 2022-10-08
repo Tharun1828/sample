934a966d0a40d8ae577ed238ca86541f97e086a3
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2022-10-08 21:49:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/SadKidBGMZ/bgm_auto_filter_bot.git /bgm_auto_filter_bot
+  git clone https://github.com/Tharun1828/namba.git /namba
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /bgm_auto_filter_bot
+  git clone $UPSTREAM_REPO /namba
 fi
-cd /bgm_auto_filter_bot
+cd /namba
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
